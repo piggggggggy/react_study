211e3b8c8e69f4b8bc2408c40ae44af0232fa03e
YONG-TAE PARK <pyt4105@gmail.com>
2021-06-26 10:59:06 +0900

    리액트강의_1주차_마무리

--- src/App.js
+++ src/App.js
@@ -90,52 +90,13 @@
 
 //----------------------------------------------------------
 
-// import React from 'react';
-// import logo from './logo.svg';
-// // BucketList 컴포넌트를 import 해옵니다.
-// // import [컴포넌트 명] from [컴포넌트가 있는 파일경로];
-// import BucketList from './BucketList';
-// import './style.css';
-
-// // 클래스형 컴포넌트는 이렇게 생겼습니다!
-// class App extends React.Component {
-
-//   constructor(props){
-//     super(props);
-//     // App 컴포넌트의 state를 정의해줍니다.
-//     this.state = {
-//       list: ['권설민', '김진태', '이경원'],
-//     };
-//   }
-
-//   // 랜더 함수 안에 리액트 엘리먼트를 넣어줍니다!
-//   render() {
-//     // this 키워드를 통해 state에 접근할 수 있어요.
-//     console.log(this.state);
-
-//       return (
-//       <div className="App">
-//         <div className="container">
-//             <h1 className="title">내 버킷리스트</h1>
-//             <hr className="line"/>
-//             {/* 컴포넌트를 넣어줍니다. */}
-//             {/* <컴포넌트 명 [props 명]={넘겨줄 것(리스트, 문자열, 숫자, ...)}/> */}
-//             <BucketList list={this.state.list} />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
-//---------------------숙제----------------------------
-
 import React from 'react';
 import logo from './logo.svg';
 // BucketList 컴포넌트를 import 해옵니다.
 // import [컴포넌트 명] from [컴포넌트가 있는 파일경로];
-import Friends from './Friends';
+import BucketList from './BucketList';
+// import './style.css';
+import "./scss_ex.scss"; 
 
 // 클래스형 컴포넌트는 이렇게 생겼습니다!
 class App extends React.Component {
@@ -144,19 +105,60 @@ class App extends React.Component {
     super(props);
     // App 컴포넌트의 state를 정의해줍니다.
     this.state = {
-      name: '벤'
+      list: ['권설민', '김진태', '이경원'],
     };
   }
 
   // 랜더 함수 안에 리액트 엘리먼트를 넣어줍니다!
   render() {
     // this 키워드를 통해 state에 접근할 수 있어요.
+    console.log(this.state);
+
       return (
       <div className="App">
-        <Friends name={this.state.name}/>
+        <div className="container">
+            <h1 className="title">내 버킷리스트</h1>
+            <hr className="line"/>
+            {/* 컴포넌트를 넣어줍니다. */}
+            {/* <컴포넌트 명 [props 명]={넘겨줄 것(리스트, 문자열, 숫자, ...)}/> */}
+            <BucketList list={this.state.list} />
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+//---------------------숙제----------------------------
+
+// import React from 'react';
+// import logo from './logo.svg';
+// // BucketList 컴포넌트를 import 해옵니다.
+// // import [컴포넌트 명] from [컴포넌트가 있는 파일경로];
+// import Friends from './Friends';
+
+
+// // 클래스형 컴포넌트는 이렇게 생겼습니다!
+// class App extends React.Component {
+
+//   constructor(props){
+//     super(props);
+//     // App 컴포넌트의 state를 정의해줍니다.
+//     this.state = {
+//       name: '벤'
+//     };
+//   }
+
+//   // 랜더 함수 안에 리액트 엘리먼트를 넣어줍니다!
+//   render() {
+//     // this 키워드를 통해 state에 접근할 수 있어요.
+//       return (
+//       <div className="App">
+//         <Friends name={this.state.name}/>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
